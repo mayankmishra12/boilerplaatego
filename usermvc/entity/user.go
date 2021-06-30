353d5c383a5a0f2587a3845be5646abbcb0f2765
mayank <mayank@mayanks-MacBook-Air.local>
2021-06-30 06:31:06 +0530

    adding getallAccountDetails

--- usermvc/entity/user.go
+++
@@ -1,8 +0,0 @@
-package entity
-
-type User struct {
-	Email        string `json:"email"`
-	FirstName    string `json:"firstName"`
-	LastName     string `json:"lastName"`
-	Entitlements string `json:"entitlements,omitempty"`
-}
--- usermvc/model/Accountdetailsresponse.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type AccountDetailsResponse struct {
-	StatusCode  int `json:"status_code"`
-	Payload interface{} `json:"payload"`
-}
